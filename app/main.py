31fa273872af5d60f43c4d56602eb49526a1fab0
KendallA <KendallA@svillemgt.global.fujitsu.com>
2023-11-25 12:02:15 +0000

    react server

--- app/main.py
+++ app/main.py
@@ -7,11 +7,13 @@ from openai import OpenAI
 import functions
 import yaml
 import logging
+from fastapi.middleware.cors import CORSMiddleware
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-logger.info('~~~~~~~~~~~~~~~~~LOGGING TEST ~~~~~~~~~~~~~~~~~~')
+logger.info('~~~~~~~~~~~~~~~~~LOGGING TES5 ~~~~~~~~~~~~~~~~~~')
 print("~~~~~~~~~~~~PRINT TEST~~~~~~~~~~")
 
 
@@ -54,6 +56,15 @@ else:
 
 # Start app
 app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 
 # Init client
